5b15e13a05ec828c1d1a2eaa803f5bfaf5ce08f5
maurya0607 <vishalmauryajnp0607@gmail.com>
2026-02-11 21:23:21 +0530

    upadate my code

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA0iJGXf1lhhYCTDPpgJ0aCLJldRs117xI",
+//   authDomain: "puzzle-game-9dd52.firebaseapp.com",
+//   projectId: "puzzle-game-9dd52",
+//   storageBucket: "puzzle-game-9dd52.firebasestorage.app",
+//   messagingSenderId: "495279168286",
+//   appId: "1:495279168286:web:4a420458d3e54dacb5870f",
+//   measurementId: "G-6V9NM8YWE4"
+// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyA0iJGXf1lhhYCTDPpgJ0aCLJldRs117xI",
   authDomain: "puzzle-game-9dd52.firebaseapp.com",
   projectId: "puzzle-game-9dd52",
   storageBucket: "puzzle-game-9dd52.firebasestorage.app",
   messagingSenderId: "495279168286",
-  appId: "1:495279168286:web:4a420458d3e54dacb5870f",
-  measurementId: "G-6V9NM8YWE4"
+  appId: "1:495279168286:web:500c00cf0efd923ab5870f",
+  measurementId: "G-V3E2XKXJYE"
 };
 
 const app = initializeApp(firebaseConfig);
